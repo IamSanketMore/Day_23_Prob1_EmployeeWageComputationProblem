143cb89dd808493544a14e1f4993cc025c6fe1b1
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-26 22:42:05 +0530

    Add: Day23 EmpWage Computtaion in Master branch

---
+++ EmpWageComputation.java
@@ -0,0 +1,9 @@
+public class EmpWageComputation
+{
+	public static void main(String[] args)
+        {
+                System.out.println("--------------------------------------------------------------------");
+                System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+                System.out.println("--------------------------------------------------------------------");
+	}
+}
